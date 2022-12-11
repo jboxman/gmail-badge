d2eda4b4c30eed453127d094a1ee3d9084afae9e
Jason Boxman <jasonb@edseek.com>
2022-12-11 01:20:36 -0500

    Update meta

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import * as VM from '@violentmonkey/dom';
 
 VM.observe(document.head, () => {
   const $el = document.querySelector('title');
+  // https://github.com/developit/gmail-unread-count-badge/blob/main/src/content.js
   let m = String($el.innerText).match(/Inbox(?: \((\d+)\))? -/);
   if (m) navigator.setAppBadge(m[1] | 0 || null);
 });
